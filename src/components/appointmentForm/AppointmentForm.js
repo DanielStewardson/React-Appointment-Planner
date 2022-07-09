f058fd6b9f0aaaf32a347ecf7a3040dcf3a27202
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-07-09 15:56:31 +0100

    build appointment page form

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from '../contactPicker/ContactPicker';
 
 export const AppointmentForm = ({
   contacts,
@@ -20,6 +21,19 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="title">Appointment name:</label><br/>
+      <input type='text' value={title} id='title' onChange={(e) => setTitle(e.target.value)}></input>
+
+      <label htmlFor="date">Date:</label><br/>
+      <input type='date' value={date} id='date' min={getTodayString()} onChange={(e) => setDate(e.target.value)}></input>
+
+      <label htmlFor="time">Time:</label><br/>
+      <input type='time' value={time} id='time' onChange={(e) => setTime(e.target.value)}></input>
+
+      <ContactPicker contacts={contacts} setContact={setContact} />
+
+      <input type='submit' value='Add appointment'></input>
+    </form>
   );
 };
