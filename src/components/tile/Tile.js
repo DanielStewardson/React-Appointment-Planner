a3287488e613f59cd05db16f119ac9b7cda5a96f
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-07-09 15:58:47 +0100

    build tile list component to render data input

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ data }) => {
+  const tileData = Object.values(data);
+
   return (
     <div className="tile-container">
-      
+      {
+        tileData.map((element, index) => {
+          if (index === 0){
+          return <p key={index} className='tile-title'>{element}</p>
+          }
+          return <p key={index} className='tile'>{element}</p>
+        })
+      }
     </div>
   );
 };
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ data, emptyMessage }) => {
+  if (!data || data.length === 0) {
+    return (
+      <div>
+         <p>{emptyMessage}</p>
+      </div>
+      )
+  }
   return (
     <div>
-      
+      {
+        data.map((element, index) => {
+          return <Tile key={index} data={element} />
+        })
+      }
     </div>
   );
 };
