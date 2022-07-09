f6530155ac7ad62077d809d8a017560222f5bddd
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-07-09 15:56:42 +0100

    build contact page form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,17 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label><br/>
+      <input type='text' id='name' value={name} onChange={(e) => setName(e.target.value)}></input>
+
+      <label htmlFor="phone">UK Phone number:</label><br/>
+      <input type='tel' id='phone' pattern='[0][0-9]{10}$' value={phone} onChange={(e) => setPhone(e.target.value)}></input>
+
+      <label htmlFor="email">Email address:</label><br/>
+      <input type='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
+
+      <input type='submit' value='Save contact'></input>
+    </form>
   );
 };
