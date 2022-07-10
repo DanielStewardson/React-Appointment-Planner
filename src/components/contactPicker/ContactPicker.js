c8c811737fae4e668315b893ae10ce243b99b334
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-07-10 18:47:09 +0100

    fix contact picker on appointment form

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-export const ContactPicker = ({ contacts, setContact }) => {
+export const ContactPicker = ({ contacts, contact, setContact }) => {
   return (
     <>
       <label htmlFor='contacts'>Contact:</label>
-      <select id='contacts' onChange={(e) => setContact(e.target.value)}>
-        <option key='default' defaultValue='' disabled>Select contact details</option>
-
-          {contacts.map((contact, index) => {
+      <select id='contacts' value={contact} onChange={(e) => setContact(e.target.value)}>
+        <option>Select contact details</option>
+          {contacts.map((contact) => {
             return (
               <option key={contact.name} value={contact.name}>{contact.name}</option>
             )
