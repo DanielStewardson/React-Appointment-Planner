96f6a223adbb608386c66c7bd32a2f171629b907
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-07-16 20:14:01 +0100

    add show details onClick to tiles

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { DeleteButton } from "../deleteButton/DeleteButton";
 
-export const Tile = ({ data, id, handleDelete }) => {
+export const Tile = ({ data, showDetails }) => {
   const tileData = Object.values(data);
 
+  if (typeof data === 'string') {
+    return (
+      <div className="tile" onClick={() => showDetails(data)}>
+        <p className='tile-title'>{data}</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="tile-container">
-      <DeleteButton id={id} handleDelete={handleDelete} />
+    <div className="tile" onClick={() => alert(tileData)}>
       {
         tileData.map((element, index) => {
-          if (index === 0){
-          return <p key={index} className='tile-title'>{element}</p>
+          if (index === 0) {
+            return <p key={index} className='tile-title'>{element}</p>
           }
-          return <p key={index} className='tile'>{element}</p>
+            return <p key={index} className='tile-text'>{element}</p>
         })
       }
     </div>
@@ -20,3 +26,4 @@ export const Tile = ({ data, id, handleDelete }) => {
 };
 
 // Make notes a pop out on click rather then straight render
+//make onclick run passed down function from contacts, appoints etc. that will open modal
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tile } from "../tile/Tile";
 
-export const TileList = ({ data, emptyMessage, handleDelete }) => {
+export const TileList = ({ data, emptyMessage, showDetails }) => {
   if (!data || data.length === 0) {
     return (
       <div>
@@ -13,7 +13,7 @@ export const TileList = ({ data, emptyMessage, handleDelete }) => {
     <>
       {
         data.map((element, index) => {
-          return <Tile key={index} data={element} handleDelete={handleDelete} id={index}/>
+          return <Tile key={index} data={element} showDetails={showDetails} />
         })
       }
     </>
