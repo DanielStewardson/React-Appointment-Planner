56ee37a00b886f92e6022465f63d23db5f589713
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-07-25 09:36:20 +0100

    Add scroll to contact details container when content is to large

--- src/components/contactDetails/ContactDetails.js
+++ src/components/contactDetails/ContactDetails.js
@@ -4,26 +4,26 @@ import './contactDetails.css';
 export const ContactDetails = ({ contactDetails, handleDelete, handleEditContact, closeContactDetails }) => {
     return (
         <div className='contact-details-container'>
-            <div className='contact-details-content'>
-                <div className='contact-details-header'>
-                    <h3 className='contact-details-name'>{contactDetails.name}</h3>
-                    <button className='close-item-button' onClick={closeContactDetails}>  &#x2715;  </button>
+            <div className='contact-details-header'>
+                <h3 className='contact-details-name'>{contactDetails.name}</h3>
+                <button className='close-item-button' onClick={closeContactDetails}>  &#x2715;  </button>
+            </div>
+
+            <div className='contact-details-body'>
+                <div className='contact-details-item'>
+                    <h4>Phone</h4>
+                    <p>{contactDetails.phone}</p>
                 </div>
-                <div className='contact-details-body'>
-                    <div className='contact-details-item'>
-                        <h4>Phone</h4>
-                        <p>{contactDetails.phone}</p>
-                    </div>
-                    <div className='contact-details-item'>
-                        <h4>Email</h4>
-                        <p>{contactDetails.email}</p>
-                    </div>
-                    <div className='contact-details-item'>
-                        <h4>Notes</h4>
-                        <p>{contactDetails.notes}</p>
-                    </div>
+                <div className='contact-details-item'>
+                    <h4>Email</h4>
+                    <p>{contactDetails.email}</p>
+                </div>
+                <div className='contact-details-item'>
+                    <h4>Notes</h4>
+                    <p>{contactDetails.notes}</p>
                 </div>
             </div>
+
             <div className='contact-details-footer'>
                 <button 
                     className='contact-details-button edit' 
