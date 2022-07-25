cf62f5d0c3515036d63a3d5cb98708d28a467d83
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-07-25 09:41:23 +0100

    Build Appointment details comp ( make shared reusable with contact details )

---
+++ src/components/appointmentDetails/appointmentDetails.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './appointmentDetails.css';
+
+export const AppointmentDetails = ({ appointmentDetails, handleDelete, handleEditAppointment, closeAppointmentDetails }) => {
+    return (
+        <div className='appointment-details-container'>
+                <div className='appointment-details-header'>
+                    <h3 className='appointment-details-name'>{appointmentDetails.title}</h3>
+                    <button className='close-item-button' onClick={closeAppointmentDetails}>  &#x2715;  </button>
+                </div>
+
+                <div className='appointment-details-body'>
+                    <div className='appointment-details-item'>
+                        <div className='appointment-details-dateAndTime'>
+                            <p>Date: {appointmentDetails.date}</p>
+                            <p>Time: {appointmentDetails.time}</p>
+                        </div>
+                    </div>
+                    <div className='appointment-details-item'>
+                        <h4>Contact</h4>
+                        <p>{appointmentDetails.contact}</p>
+                    </div>
+                    <div className='appointment-details-item'>
+                        <h4>Notes</h4>
+                        <p>{appointmentDetails.notes}</p>
+                    </div>
+                </div>
+
+            <div className='appointment-details-footer'>
+                <button 
+                    className='appointment-details-button edit' 
+                    onClick={() => {handleEditAppointment(appointmentDetails.title)}}
+                    >
+                        Edit appointment
+                </button>
+                <button 
+                    className='appointment-details-button delete' 
+                    onClick={() => {handleDelete(appointmentDetails.title)}}
+                    >
+                        Delete appointment
+                </button>
+            </div>
+        </div>
+    )
+};
