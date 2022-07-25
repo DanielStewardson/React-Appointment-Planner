cbf4ad6d327e5abd50313845fbda0c238c3eb62b
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-07-25 14:27:13 +0100

    Change header to show Home on home page

--- src/components/pageHeaders/header.js
+++ src/components/pageHeaders/header.js
@@ -2,6 +2,13 @@ import React from "react";
 import './header.css';
 
 export const Header = ({ pageName }) => {
+    if (!pageName) {
+        return (
+            <div className='page-header'>
+                <h1>Dan's - Digi - Diary</h1>
+            </div>
+        );
+    }
     return ( 
         <div className='page-header'>
             <h1>{pageName}</h1>
